be53f4b6a643f1faadfa1a99c136301fa6945b90
shamshu <shamshuddinc1@gmail.com>
2019-03-27 09:01:15 +0530

    changes done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  show:boolean;
+  buttonName="show";
+
+  ngOnInit() { }
+
+  toggle(){
+    this.show = !this.show
+    //if we need show and hide buttons 
+    if(this.show){
+      this.buttonName = "hide";
+    }
+    else{
+       this.buttonName = "show"
+    }
+  
+  }
 }
